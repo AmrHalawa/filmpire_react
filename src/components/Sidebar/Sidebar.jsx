774513da612621e075a6929dfca93f229f79eb6c
Amr Halawa <amrhalawa292@gmail.com>
2024-09-01 12:39:54 +0800

    fetch movie genres

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -12,6 +12,7 @@ import {
 import { Link } from "react-router-dom";
 import { useTheme } from "@mui/styles";
 
+import { useGetGenresQuery } from "../../services/TMDB";
 import useStyles from "./styles";
 
 const categories = [
@@ -35,6 +36,9 @@ const blueLogo =
 const Sidebar = ({ setMobileOpen }) => {
   const theme = useTheme();
   const classes = useStyles;
+  const { data, isFetching, error } = useGetGenresQuery();
+
+  console.log(data);
   return (
     <>
       <Link to="/" className={classes.imageLink}>
@@ -65,20 +69,26 @@ const Sidebar = ({ setMobileOpen }) => {
       <Divider />
       <List>
         <ListSubheader>Genres</ListSubheader>
-        {demoCategories.map(({ label, value }) => (
-          <Link key={value} className={classes.links} to="/">
-            <ListItem onClick={() => {}} button>
-              {/* <ListItemIcon>
+        {isFetching ? (
+          <Box display="flex" justifyContent="center">
+            <CircularProgress />
+          </Box>
+        ) : (
+          data.genres.map(({ name, id }) => (
+            <Link key={name} className={classes.links} to="/">
+              <ListItem onClick={() => {}} button>
+                {/* <ListItemIcon>
                 <img
                   src={redLogo}
                   className={classes.genreImage}
                   height={30}
                 ></img>
               </ListItemIcon> */}
-              <ListItemText primary={label} />
-            </ListItem>
-          </Link>
-        ))}
+                <ListItemText primary={name} />
+              </ListItem>
+            </Link>
+          ))
+        )}
       </List>
     </>
   );
